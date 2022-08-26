1d61f998133b8d42999cc4009cf1d060c78ac4b2
Carles Roch-Cunill <carles.roch-cunill@gov.bc.ca>
2022-08-26 15:09:45 -0700

    Customized message for f2u30s

--- client/src/components/form/Greeter.js
+++ client/src/components/form/Greeter.js
@@ -40,7 +40,7 @@ console.log(values)
             disabled={isDisabled}
             options={[
               { value: 'Aloha', label: 'Aloha' },
-              { value: 'Bonjour', label: 'Carles diu Bondia' },
+              { value: 'Bonjour', label: 'Carles diu Bondia  a f2u30s' },
               { value: 'Bonjour', label: 'Bonjour' },
               { value: 'Greetings and salutations', label: 'Greetings and salutations' },
               { value: 'Hello', label: 'Hello' },
