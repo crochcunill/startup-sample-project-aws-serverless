4fad36bd9b3fef6ca199172cc6d28e092ca74043
Carles Roch-Cunill <45524596+crochcunill@users.noreply.github.com>
2023-02-13 15:22:31 -0800

    Change greeter message for m9tvhw-dev

--- client/src/components/form/Greeter.js
+++ client/src/components/form/Greeter.js
@@ -40,7 +40,7 @@ console.log(values)
             disabled={isDisabled}
             options={[
               { value: 'Aloha', label: 'Aloha' },
-              { value: 'Bonjour', label: 'Serverless app for ubwyx8-dev - Feb 7, 2023' },
+              { value: 'Bonjour', label: 'Serverless app for m9tvhw-dev = Upgrade to 1.5.5- Feb 13, 2023' },
               { value: 'Bonjour', label: 'Bonjour' },
               { value: 'Greetings and salutations', label: 'Greetings and salutations' },
               { value: 'Hello', label: 'Hello' },
