dbbe9e3696c5a725c30a22df4375b8618c84e349
Carles Roch-Cunill <45524596+crochcunill@users.noreply.github.com>
2023-02-22 19:01:55 -0800

    Updating the greeting for td5cou

--- client/src/components/form/Greeter.js
+++ client/src/components/form/Greeter.js
@@ -40,7 +40,7 @@ console.log(values)
             disabled={isDisabled}
             options={[
               { value: 'Aloha', label: 'Aloha' },
-              { value: 'Bonjour', label: 'Serverless app Upgrade to 1.5.5' },
+              { value: 'Bonjour', label: 'Serverless - td5cou - Upgrade to 1.5.5 Feb 22, 2023' },
               { value: 'Bonjour', label: 'Bonjour' },
               { value: 'Greetings and salutations', label: 'Greetings and salutations' },
               { value: 'Hello', label: 'Hello' },
